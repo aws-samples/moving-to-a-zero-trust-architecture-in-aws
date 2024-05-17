7025d7cb5405a37bedb335d99d4d479d839cbf94
Adam Palmer <adampalm@amazon.co.uk>
2024-05-17 09:30:06 +0100

    initial application commit

---
+++ applications/mservice1/app1.py
@@ -0,0 +1,177 @@
+# Import Modules
+
+from flask import Flask, request
+import requests
+from botocore.awsrequest import AWSRequest
+from botocore.credentials import Credentials
+from botocore.auth import SigV4Auth
+import botocore.session
+import json
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+# Define Variables
+mservice2 = "https://mservice2.backend.pablosc.people.aws.dev"
+mservice2sec = "https://mservice2.backend.pablosc.people.aws.dev/secure"
+
+# Secure signer function
+
+def signer(endpoint):
+  session = botocore.session.Session()
+  signer = SigV4Auth(session.get_credentials(), 'vpc-lattice-svcs', 'eu-west-1')
+  endpoint = endpoint
+  data = "null"
+  headers = {}
+  request = AWSRequest(method='GET', url=endpoint, data=data, headers=headers)
+  request.context["payload_signing_enabled"] = False # payload signing is not supported
+  signer.add_auth(request)
+  prepped = request.prepare()
+  return prepped
+
+app = Flask(__name__)
+
+@app.route("/",  methods=['GET'])
+def app1resp():
+
+    try:    
+        requestdata = request.json
+        m2aresp = requests.get(mservice2,json=requestdata)
+        m2payload = json.loads(m2aresp.content)
+
+        data = {
+            'app1headers'       :   dict(request.headers),
+            'app1url'           :   request.url,
+            'app1message'       :   "Welcome to mservice1 - this is a simple open API - it calls mservice2",
+            'app1information'   :   "Meeoow - Make a connection to the secure endpoint for secrets",
+            'app2headers'       :   m2payload['app2headers'],
+            'app2url'           :   m2aresp.url,
+            'app2message'       :   m2payload['app2message'],
+            'app2information'   :   m2payload['app2information'],
+            'app2status'        :   m2aresp.status_code
+        }
+
+        if requestdata['portalidname'] == 'Anonymous':
+            resp = 418
+        else:
+            resp = 200
+        
+        response = app.response_class(
+            response=json.dumps(data),
+            status=resp,
+            mimetype='application/json'
+        )
+        return response
+    
+    except Exception as e:
+        logger.info(e)
+
+        data = {
+            'app1headers'       :   '',
+            'app1url'           :   '',
+            'app1message'       :   '',
+            'app1information'   :   '',
+            'app2headers'       :   '',
+            'app2url'           :   '',
+            'app2message'       :   '',
+            'app2information'   :   '',
+            'app2status'        :   ''
+        }
+
+        response = app.response_class(
+            response=json.dumps(data),
+            status=500,
+            mimetype='application/json'
+        )
+        return response
+
+@app.route("/secure", methods=['GET','POST'])
+def app1respsec():
+    
+    try:    
+        headers = dict(request.headers)
+
+    ## Comment out the below line when switching to signed requests
+        m2asecresp = requests.get(mservice2sec)
+    ## Uncomment the below line when switching to signed requests
+        #prepped = signer(mservice2sec)
+        #m2asecresp = requests.get(prepped.url, headers=prepped.headers)
+            
+        m2secpayload = json.loads(m2asecresp.content)
+    
+        if "X-Amz-Content-Sha256" in headers:
+            resp = 200
+            data = {
+                'app1secmessage'        :   'Welcome to mservice1 (secure) - this contains sensitive cat treat information',
+                'app1secinformation'    :   'There is a cup of milk that im sure would enjoy',
+                'app1secdetails'        :   'Sit back, take a load off and enjoy',
+                'app1secheaders'        :   dict(request.headers),
+                'app1securl'            :   request.url,
+                'app2secmessage'        :   m2secpayload['app2secmessage'],
+                'app2secinformation'    :   m2secpayload['app2secinformation'],
+                'app2secdetails'        :   m2secpayload['app2secdetails'],
+                'app2secheaders'        :   m2secpayload['app2secheaders'],
+                'app2securl'            :   m2asecresp.url,
+                'app2secstatus'         :   m2asecresp.status_code
+                }
+        else:
+            resp = 418
+            data = {
+                'app1secmessage'        :   'Welcome to mservice1 (secure) - this contains sensitive cat treat information',
+                'app1secinformation'    :   '[For more info, sign your requests]',
+                'app1secdetails'        :   '[For more info, sign your requests]',
+                'app1secheaders'        :   dict(request.headers),
+                'app1securl'            :   request.url,
+                'app2secmessage'        :   m2secpayload['app2secmessage'],
+                'app2secinformation'    :   m2secpayload['app2secinformation'],
+                'app2secdetails'        :   m2secpayload['app2secdetails'],
+                'app2secheaders'        :   m2secpayload['app2secheaders'],
+                'app2securl'            :   m2asecresp.url,
+                'app2secstatus'         :   m2asecresp.status_code
+                }
+        
+        response = app.response_class(
+            response=json.dumps(data),
+            status=resp,
+            mimetype='application/json'
+        )
+
+        return response
+    
+    except Exception as e:
+        logger.info(e)
+
+        data = {
+            'app1secmessage'        :   '',
+            'app1secinformation'    :   '',
+            'app1secdetails'        :   '',
+            'app1secheaders'        :   '',
+            'app1securl'            :   '',
+            'app2secmessage'        :   '',
+            'app2secinformation'    :   '',
+            'app2secdetails'        :   '',
+            'app2secheaders'        :   '',
+            'app2securl'            :   '',
+            'app2secstatus'         :   ''
+            }
+        
+        response = app.response_class(
+            response=json.dumps(data),
+            status=500,
+            mimetype='application/json'
+        )
+        
+        return response
+
+@app.route("/health",  methods=['GET'])
+def hc():
+    response = app.response_class(
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+if __name__ == '__main__':
+  app.run(debug=True,host="0.0.0.0",port=8081)
---
+++ applications/portal/index.py
@@ -0,0 +1,240 @@
+#import modules
+
+import os
+from flask import Flask, render_template, request, redirect, url_for
+import requests
+from botocore.awsrequest import AWSRequest
+from botocore.credentials import Credentials
+from botocore.auth import SigV4Auth
+import botocore.session
+import json
+import base64
+import jwt
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+#define variables
+
+img = os.path.join('static','images')
+global portal
+render = {}
+
+mservice1 = "https://mservice1.backend.pablosc.people.aws.dev/"
+mservice1sec = "https://mservice1.backend.pablosc.people.aws.dev/secure"
+
+avaurl = 'https://public-keys.prod.verified-access.eu-west-1.amazonaws.com/'
+
+# Secure signer function
+def signer(endpoint):
+  session = botocore.session.Session()
+  signer = SigV4Auth(session.get_credentials(), 'vpc-lattice-svcs', 'eu-west-1')
+  endpoint = endpoint
+  data = "null"
+  headers = {}
+  request = AWSRequest(method='GET', url=endpoint, data=data, headers=headers)
+  request.context["payload_signing_enabled"] = False # payload signing is not supported
+  signer.add_auth(request)
+  prepped = request.prepare()
+  return prepped
+
+# AVA depacker function
+def depacker(portal):
+  
+  if "X-Amzn-Ava-User-Context" in portal:
+
+    #Decode the headers
+    encoded_jwt = portal['X-Amzn-Ava-User-Context']
+    jwt_headers = encoded_jwt.split('.')[0]
+    decoded_jwt_headers = base64.b64decode(jwt_headers)
+    decoded_jwt_headers = decoded_jwt_headers.decode("utf-8")
+    decoded_json = json.loads(decoded_jwt_headers)
+    kid = decoded_json['kid']
+    
+    #Get the public key
+    url = avaurl + kid
+    req = requests.get(url)
+    pub_key = req.text
+    
+    #Get the payload
+    payload = jwt.decode(encoded_jwt, pub_key, algorithms=['ES384'])
+  
+    ptype   = os.path.join(img,'verifiedaccess.png')
+    pid     = os.path.join(img, payload['user']['user_name'] + '.png')
+    pidname = payload['user']['user_name']
+
+  else:
+    ptype = os.path.join(img, 'clientvpn.png')
+    pid   = os.path.join(img, 'mrx.png')
+    pidname = "Anonymous"
+
+  response = {
+    "portaltype"    : ptype,
+    "portalid"      : pid,
+    "portalidname"  : pidname
+  }
+  return response
+
+# Portal Application
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def getBaselinePictures():
+  
+  portal = depacker(dict(request.headers))
+
+  render = {
+    "app1img"               : os.path.join(img,'wait.jpg'),
+    "app1secimg"            : os.path.join(img,'wait.jpg'),
+    "app2img"               : os.path.join(img,'wait.jpg'),
+    "app2secimg"            : os.path.join(img,'wait.jpg'),
+    'portalmessage'         : "Welcome to the Feline microservice portal",
+    'portalinformation'     : "Build a zero-trust layer to reveal feline imagery" if portal['portalidname'] == 'Anonymous' else "Welcome " + portal['portalidname'] + " - check the cat pictures (if you can) !",
+    'portalheaders'         : dict(request.headers),
+    "portaltype"            : portal['portaltype'],
+    "portalid"              : portal['portalid'],
+    "portalidname"          : portal['portalidname'],
+    'app1headers'           : '',
+    'app1url'               : '',
+    'app1status'            : '',
+    'app1message'           : '',
+    'app1information'       : '',
+    'app2headers'           : '',
+    'app2url'               : '',
+    'app2message'           : '',
+    'app2information'       : '',
+    'app2status'            : '',
+    'app1secstatus'         : '',
+    'app1secmessage'        : '',
+    'app1secinformation'    : '',
+    'app1secdetails'        : '',
+    'app1secheaders'        : '',
+    'app1securl'            : '',
+    'app2secmessage'        : '',
+    'app2secinformation'    : '',
+    'app2secdetails'        : '',
+    'app2secheaders'        : '',
+    'app2securl'            : '',
+    'app2secstatus'         : ''
+  }
+
+  return render_template('index.html', render=render)
+
+@app.route('/run', methods=['GET','POST'])
+def runapp():
+
+  portal = depacker(dict(request.headers))
+  
+  portaldata = {
+      'portalmessage'         : "Welcome to the Feline microservice portal",
+      'portalinformation'     : "Build a zero-trust layer to reveal feline imagery" if portal['portalidname'] == 'Anonymous' else "Welcome " + portal['portalidname'] + " - check the cat pictures (if you can) !",
+      'portalheaders'         : dict(request.headers),
+      'portaltype'            : portal['portaltype'],
+      'portalid'              : portal['portalid'],
+      'portalidname'          : portal['portalidname'],
+  }
+
+  try:
+    logger.info("Attempting connection to mservice1 unsecured endpoint")
+    m1aresp = requests.get(mservice1,json=portal)
+    payload = json.loads(m1aresp.content)
+
+    render1 = {
+      'app1img'               : os.path.join(img,'scaredcat.jpg') if m1aresp.status_code == 200 else os.path.join(img,'wait.jpg'),
+      'app2img'               : os.path.join(img,'scaredcat.jpg') if payload['app2status'] == 200 else os.path.join(img,'wait.jpg'),
+      'app1headers'           : payload['app1headers'],
+      'app1url'               : payload['app1url'],
+      'app1status'            : m1aresp.status_code,
+      'app1message'           : payload['app1message'],
+      'app1information'       : payload['app1information'],
+      'app2headers'           : payload['app2headers'],
+      'app2url'               : payload['app2url'],
+      'app2message'           : payload['app2message'],
+      'app2information'       : payload['app2information'],
+      'app2status'            : payload['app2status']
+    }
+    render.update(render1)
+
+  except Exception as e:
+    logger.info("There was an issue connecting to mservice1 unsecured endpoint - error deatils: " + str(e))
+
+    render1 = {
+      'app1img'               : os.path.join(img,'wait.jpg'),
+      'app2img'               : os.path.join(img,'wait.jpg'),
+      'app1headers'           : "Communication Error",
+      'app1url'               : "Communication Error",
+      'app1status'            : "Communication Error",
+      'app1message'           : "Communication Error",
+      'app1information'       : "Communication Error",
+      'app2headers'           : "Communication Error",
+      'app2url'               : "Communication Error",
+      'app2message'           : "Communication Error",
+      'app2information'       : "Communication Error",
+      'app2status'            : "Communication Error"
+    }
+    render.update(render1)
+
+  try:
+    logger.info("Attempting connection to mservice1 secured endpoint")
+  ## Comment out the below line when switching to signed requests
+    m1bresp = requests.get(mservice1sec)
+  ## Uncomment the below two lines when switching to signed requests
+    #prepped = signer(mservice1sec)
+    #m1bresp = requests.get(prepped.url,headers=prepped.headers,json=portal)
+
+    payload2 = json.loads(m1bresp.content)
+    render2 = {
+      'app1secimg'            : os.path.join(img,'catmilk.png') if m1bresp.status_code == 200 else os.path.join(img,'wait.jpg'),
+      'app2secimg'            : os.path.join(img,'catincupboard.png') if payload2['app2secstatus'] == 200 else os.path.join(img,'wait.jpg'),
+      'app1secstatus'         : m1bresp.status_code,
+      'app1secmessage'        : payload2['app1secmessage'],
+      'app1secinformation'    : payload2['app1secinformation'],
+      'app1secdetails'        : payload2['app1secdetails'],
+      'app1secheaders'        : payload2['app1secheaders'],
+      'app1securl'            : payload2['app1securl'],
+      'app2secmessage'        : payload2['app2secmessage'],
+      'app2secinformation'    : payload2['app2secinformation'],
+      'app2secdetails'        : payload2['app2secdetails'],
+      'app2secheaders'        : payload2['app2secheaders'],
+      'app2securl'            : payload2['app2securl'],
+      'app2secstatus'         : payload2['app2secstatus']
+    }
+
+    render.update(render2)
+
+  except Exception as e:
+    logger.info("There was an issue connecting to mservice1 secured endpoint - error deatils: " + str(e))
+
+    render2 = {
+      'app1secimg'            : os.path.join(img,'wait.jpg'),
+      'app2secimg'            : os.path.join(img,'wait.jpg'),
+      'app1secstatus'         : "Communication Error",
+      'app1secmessage'        : "Communication Error",
+      'app1secinformation'    : "Communication Error",
+      'app1secdetails'        : "Communication Error",
+      'app1secheaders'        : "Communication Error",
+      'app1securl'            : "Communication Error",
+      'app2secmessage'        : "Communication Error",
+      'app2secinformation'    : "Communication Error",
+      'app2secdetails'        : "Communication Error",
+      'app2secheaders'        : "Communication Error",
+      'app2securl'            : "Communication Error",
+      'app2secstatus'         : "Communication Error"
+    }
+
+    render.update(render2)
+
+  render.update(portaldata)
+  return render_template('index.html', render=render)
+
+@app.route("/health",  methods=['GET'])
+def hc():
+    response = app.response_class(
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+if __name__ == '__main__':
+  app.run(debug=True,host="0.0.0.0",port=8080)
